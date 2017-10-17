fb80e76aa10021abab149e8fa71616147335132a
Diego Francklin Martins dos Santos <dfrancklin23@gmail.com>
2017-10-17 15:05:41 -0200

    [update]

--- vendors/fw/src/security/SecurityService.php
+++ vendors/fw/src/security/SecurityService.php
@@ -11,8 +11,11 @@ class SecurityService implements ISecurityService {
 
 	private $appId;
 
+	private $config;
+
 	public function __construct() {
-		$this->appId = Config::getInstance()->get('app-id');
+		$this->config = Config::getInstance();
+		$this->appId = $this->config->get('app-id');
 	}
 
 	public function isAuthenticated() : bool {
@@ -20,11 +23,51 @@ class SecurityService implements ISecurityService {
 			return false;
 		}
 
+		// If there is no user is on session...
 		if (is_null($this->getUserProfile())) {
-			return false;
+			// ...look for cookies
+			if (!$this->validateCookies()) {
+				// If there is no user is on session
+				// and no cookies was found,
+				// there is no user logged in
+				return false;
+			}
 		}
 
 		return true;
+	}
+
+	private function validateCookies() {
+		if (!array_key_exists($this->appId, $_COOKIE) || !array_key_exists('token', $_COOKIE[$this->appId])) {
+			return;
+		}
+
+		$context = $this->config->get('context');
+		$secretKey = $this->config->get('secret-key');
+		$expiration = $this->config->get('valid-time-token');
+
+		$prefix = sha1($secretKey);
+		$secretKeyEnconded = base64_encode($secretKey);
+		$token = $_COOKIE[$this->appId]['token'];
+
+		// Remove prefix
+		$token = substr($token, strlen($prefix));
+		// Base64 decode
+		$token = base64_decode($token);
+		// Remove secret
+		$payload = substr($token, strlen($secretKeyEnconded));
+		// Base64 decode
+		$payload = base64_decode($payload);
+		// Remove 'payload='
+		$userProfile = substr($payload, strlen('payload='));
+		// Unserialize profile
+		$userProfile = unserialize($userProfile);
+
+		if ($userProfile) {
+			return $this->authenticate($userProfile, true);
+		}
+
+		return false;
 	}
 
 	public function getUserProfile() {
@@ -74,13 +117,28 @@ class SecurityService implements ISecurityService {
 			return false;
 		}
 
-		$_SESSION[$this->appId]['user-profile'] = serialize($userProfile);
+		$userProfile = serialize($userProfile);
+		$_SESSION[$this->appId]['user-profile'] = $userProfile;
 
 		if ($remember) {
-
+			$this->setCookie($userProfile);
 		}
 
 		return true;
+	}
+
+	private function setCookie($userProfile) {
+		$context = $this->config->get('context');
+		$secretKey = $this->config->get('secret-key');
+		$expiration = $this->config->get('valid-time-token');
+
+		$payload = base64_encode('payload=' . $userProfile);
+		$secretKeyEnconded = base64_encode($secretKey);
+		$prefix = sha1($secretKey);
+
+		$token = $prefix . base64_encode($secretKeyEnconded . $payload);
+
+		setcookie($this->appId . '[token]', $token, time() + $expiration, $context);
 	}
 
 	public function logout() {
@@ -95,6 +153,18 @@ class SecurityService implements ISecurityService {
 		if (!count($_SESSION)) {
 			session_destroy();
 		}
+
+		$this->unsetCookie();
+	}
+
+	private function unsetCookie() {
+		if (!array_key_exists($this->appId, $_COOKIE) || !array_key_exists('token', $_COOKIE[$this->appId])) {
+			return;
+		}
+
+		$context = $this->config->get('context');
+
+		setcookie($this->appId . '[token]', null, time() - 1000, $context);
 	}
 
 }
